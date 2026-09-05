5a40b99c61a7d0e3d08d26af86de58871a82ac63
Rushdi Bahadoor <rushdi@velya.team>
2026-09-05 14:42:48 +0400

    Simple component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1 className="heading">Rushdi</h1>
+      </div>
+      <Footer />
+    </>
+    
+  )
+}
